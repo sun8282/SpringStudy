0f028dff6d477ebfab7289a29d38305294acc964
sun8282 <109707901+sun8282@users.noreply.github.com>
2025-02-24 00:01:36 +0900

    edit: 02-24 정책 변경

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -18,11 +19,11 @@ public class AppConfig {
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(MemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(MemberRepository(), discountPolicy());
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 }

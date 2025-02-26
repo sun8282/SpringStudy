09c29a82bb37755e16972cdbdc4e7f454ffb902e
SeungSoo Ha <109707901+sun8282@users.noreply.github.com>
2025-02-26 12:30:51 +0900

    edit: 테스트 코드 오류 해결

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -7,10 +7,13 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
-        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 
 }
--- core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -3,14 +3,15 @@ package hello.core.discount;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
-public class FixDiscountPolicy implements DiscountPolicy{
+public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000; //1000원 할인
 
     @Override
     public int discount(Member member, int price) {
-        if(member.getGrade() == Grade.VIP){
+        if (member.getGrade() == Grade.VIP) {
+
             return discountFixAmount;
-        }else {
+        } else {
             return 0;
         }
     }
